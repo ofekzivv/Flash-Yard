d07ac46956fae44282db56d77a3a7ea691990aa6
ofekziv <ofekziv21@gmail.com>
2021-08-29 11:50:09 +0300

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Welcome to flash-yard</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
